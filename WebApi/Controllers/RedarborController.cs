d57caf429355a8c863a4889805822ec21bf53d85
raul <raul_lorca@hotmail.com>
2019-02-25 03:17:57 +0100

    + Verbs controller + fill interface IEmployeeService + create methods EmployeeService

--- WebApi/Controllers/RedarborController.cs
+++ WebApi/Controllers/RedarborController.cs
@@ -3,12 +3,116 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
+using System.Web.Http.Description;
+using WebApi.Models;
+using WebApi.Services;
 
 namespace WebApi.Controllers
 {
     public class RedarborController : ApiController
     {
+        IEmployeeService _service;
+
+        public RedarborController()
+        {
+            _service = new EmployeeService();
+        }
+
+        [HttpGet]
+        public async Task<IQueryable<Employee>> Get()
+        {
+            return await _service.GetAll();
+        }
+
+
+        [HttpGet]
+        [ResponseType(typeof(Employee))]
+        public async Task<IHttpActionResult> Get(int id)
+        {
+            var employee = await _service.Get(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(employee);
+        }
+
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> Put(int id, Employee employee)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != employee.Id)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                await _service.Update(id, employee);
+            }
+            catch
+            {
+                if (!(await _service.EmployeeExists(id)))
+                {
+                    return NotFound();
+                }
+                return BadRequest();
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        [ResponseType(typeof(Employee))]
+        public async Task<IHttpActionResult> Post(Employee employee)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                _service.Insert(employee);
+            }
+            catch
+            {
+                if (await _service.EmployeeExists(employee.Id))
+                {
+                    return Conflict();
+                }
+                throw;
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = employee.Id }, employee);
+        }
+
+        [ResponseType(typeof(Employee))]
+        public async Task<IHttpActionResult> Delete(int id)
+        {
+            var employee = await _service.Get(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            try
+            {
+                await _service.Delete(id);
+                return Ok(employee);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+
+
+        }
 
     }
 }
---
+++ WebApi/Services/EmployeeService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using WebApi.Models;
+
+namespace WebApi.Services
+{
+    public interface IEmployeeService
+    {
+        Task<Employee> Get(int id);
+        Task<IQueryable<Employee>> GetAll();
+        Task Insert(Employee employee);
+        Task Update(int id, Employee employee);
+        Task Delete(int id);
+        Task<bool> EmployeeExists(int id);
+    }
+
+    public class EmployeeService : IEmployeeService
+    {
+        public Task Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> EmployeeExists(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Employee> Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IQueryable<Employee>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Insert(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(int id, Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
